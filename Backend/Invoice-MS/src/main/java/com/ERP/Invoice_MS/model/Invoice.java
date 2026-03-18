c11c0ab17de3f8ab62d89f8a83c8a243b3d907aa
arjun-s-15 <iamarjun9003@gmail.com>
2026-03-18 08:05:02 +0530

    added invoice template with css

--- Backend/Invoice-MS/src/main/java/com/ERP/Invoice_MS/model/Invoice.java
+++ Backend/Invoice-MS/src/main/java/com/ERP/Invoice_MS/model/Invoice.java
@@ -8,6 +8,11 @@ public class Invoice {
 //Invoice	StockCode	Description	Quantity	InvoiceDate	Price	Customer ID	Country
     private String invoiceNumber;
     private String customerName;
+    private String productId;
+    private double amount;
+    private double cgst;
+    private double sgst;
+    private double total;
 
 
 }
