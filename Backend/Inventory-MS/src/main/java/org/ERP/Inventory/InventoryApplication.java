7356a32d422c923c01f227c9f2e0e857da621bbd
arjun-s-15 <iamarjun9003@gmail.com>
2026-05-05 00:07:50 +0530

    java code commit

--- Backend/Inventory-MS/src/main/java/org/ERP/Inventory/InventoryApplication.java
+++ Backend/Inventory-MS/src/main/java/org/ERP/Inventory/InventoryApplication.java
@@ -1,7 +1,13 @@
 package org.ERP.Inventory;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
+@SpringBootApplication
+@EnableRetry
 public class InventoryApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(InventoryApplication.class, args);
     }
 }
